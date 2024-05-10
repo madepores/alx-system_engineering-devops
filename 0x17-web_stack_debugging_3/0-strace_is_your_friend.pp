71921ad5705108228d65ccc56a1e32fbc83b503a
madepores <martin.barindirimana@aims.ac.rw>
2024-05-10 12:03:49 +0000

    l

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
+# Find the PID of the Apache process
+pidof apache2
+
+# Attach strace to the Apache process
+strace -p <PID>
